e7283cac61ed94659f1700aa1467d68477a30704
Michael20112005 <151624551+Michael20112005@users.noreply.github.com>
2025-02-06 08:31:16 +0200

    Add files via upload

---
+++ 8Lab/src/components/Catalog/UI/Select.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Catalog.css";
+
+export function SelectFilter({ onChange, options }) {
+  return (
+    <div className="filter-container">
+      <select onChange={onChange}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
